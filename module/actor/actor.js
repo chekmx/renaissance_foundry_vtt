d51d59f79b375a583f5efc24ea2262d10eeff91e
chekmx <stephen.john.richards@gmail.com>
2021-05-29 12:22:13 +0200

    Initial commit

---
+++ module/actor/actor.js
@@ -0,0 +1,45 @@
+/**
+ * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system.
+ * @extends {Actor}
+ */
+export class BoilerplateActor extends Actor {
+
+  /**
+   * Augment the basic actor data with additional dynamic data.
+   */
+  prepareData() {
+    super.prepareData();
+
+    const actorData = this.data;
+    const data = actorData.data;
+    const flags = actorData.flags;
+
+    // Make separate methods for each Actor type (character, npc, etc.) to keep
+    // things organized.
+    if (actorData.type === 'character') this._prepareCharacterData(actorData);
+  }
+
+  /**
+   * Prepare Character type specific data
+   */
+  _prepareCharacterData(actorData) {
+    const data = actorData.data;
+
+    // Make modifications to data here. For example:
+
+    // Loop through ability scores, and add their modifiers to our sheet output.
+    for (let [key, ability] of Object.entries(data.abilities)) {
+      // Calculate the modifier using d20 rules.
+      ability.mod = Math.floor((ability.value - 10) / 2);
+    }
+    data.damageModifier = data.abilities.str.value + data.abilities.siz.value //this._calculateDamageModifier(actorData)
+    data.hitPoints = Math.ceil((data.abilities.con.value + data.abilities.siz.value) / 2)
+    data.majorWoundLevel = Math.ceil(data.hitPoints / 2)
+    console.log(data.majorWoundLevel)
+    data.movementRate = 15
+  }
+
+  _calculateDamageModifier(actorData) {
+    return data.abilities.str.value + data.abilities.siz.value
+  } 
+}
