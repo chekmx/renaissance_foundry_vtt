1725bf772c92b720b8c7d46f2a6c148e6536cea6
chekmx <stephen.john.richards@gmail.com>
2021-06-04 12:07:53 +0200

    Fix issue with failed weapon rolls

--- module/item/item.js
+++ module/item/item.js
@@ -44,7 +44,9 @@ export class BoilerplateItem extends Item {
       let skill = this.actor.items.filter( (i) => i.data.type === "skill" && i.data.name === item.data.skill)[0];
 
       console.log(skill);
-      let successDisplay = roll.result == 100 ? "FUMBLE" : roll.result <= skill.data.value / 10 ? "CRITICAL" : roll.result <= skill.data.value ? "SUCCESS" : "FAIL"
+      let successDisplay = roll.result == 100 ? "FUMBLE" 
+                                              : roll.result <= skill.data.data.value / 10 ? "CRITICAL" 
+                                              : roll.result <= skill.data.data.value ? "SUCCESS" : "FAIL"
       
       let chatData = {
         type: CHAT_MESSAGE_TYPES.ROLL,
@@ -53,7 +55,7 @@ export class BoilerplateItem extends Item {
         roll: roll,
         rollMode: game.settings.get("core", "rollMode"),
         content:  `<p><b>${successDisplay}</b><br>  
-                  Against Target <b>${skill.data.data.value}</b>
+                  Against <b>${skill.data.name}</b> skill <b>${skill.data.data.value}</b>
                   with a roll of <b>${roll.result}</b></p>
                   damage = ${damageRoll.result}
                   ${skill.data.data.description}`
@@ -62,7 +64,9 @@ export class BoilerplateItem extends Item {
     } else {
     //TO DO: Handle skills > 100.  check if auto fail for 95-99 
 
-      let successDisplay = roll.result == 100 ? "FUMBLE" : roll.result <= item.data.value / 10 ? "CRITICAL" : roll.result <= item.data.value ? "SUCCESS" : "FAIL"
+      let successDisplay = roll.result == 100 ? "FUMBLE" 
+                                              : roll.result <= item.data.value / 10 ? "CRITICAL" 
+                                              : roll.result <= item.data.value ? "SUCCESS" : "FAIL"
       
       let chatData = {
         type: CHAT_MESSAGE_TYPES.ROLL,
