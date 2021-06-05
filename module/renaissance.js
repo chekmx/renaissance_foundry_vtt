0aac5bcb6c990d6877c83cadaa64d6be92f015d6
chekmx <stephen.john.richards@gmail.com>
2021-06-05 09:47:51 +0200

    last bit of renaming

--- module/renaissance.js
+++ module/renaissance.js
@@ -3,7 +3,7 @@ import { RenaissanceActor } from "./actor/actor.js";
 import { RenaissanceCharacterSheet } from "./actor/character-sheet.js";
 import { RenaissanceCreatureSheet } from "./actor/creature-sheet.js"
 import { RenaissanceItem } from "./item/item.js";
-import { BoilerplateItemSheet } from "./item/item-sheet.js";
+import { RenaissanceItemSheet } from "./item/item-sheet.js";
 //import { _getInitiativeFormula } from "./module/combat.js";
 
 Hooks.once('init', async function() {
@@ -35,7 +35,7 @@ Hooks.once('init', async function() {
   Actors.registerSheet("renaissance", RenaissanceCharacterSheet, { types: ['character'], makeDefault: true });
   Actors.registerSheet("renaissance", RenaissanceCreatureSheet, {  types: ['creature'], makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("renaissance", BoilerplateItemSheet, { makeDefault: true });
+  Items.registerSheet("renaissance", RenaissanceItemSheet, { makeDefault: true });
 
   // If you need to add Handlebars helpers, here are a few useful examples:
   Handlebars.registerHelper('concat', function() {
