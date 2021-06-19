e48328ae5a3005fd23f545bbc2c3ccbb60417d09
chekmx <stephen.john.richards@gmail.com>
2021-06-19 11:59:58 +0200

    some null checks added and a bit of clean up

--- module/rules/getAbility.js
+++ module/rules/getAbility.js
@@ -1,5 +1,8 @@
 export function getAbility(actorData, abilityName) {
     let result = {};
+    if(actorData.data == undefined) return undefined
+    if(actorData.data.abilities == undefined) return undefined
+
     Object.entries(actorData.data.abilities).forEach((pair) => {
         if (pair[0] == abilityName) {
             result = pair[1];
--- module/rules/getBaseSkill.js
+++ module/rules/getBaseSkill.js
@@ -4,7 +4,9 @@ export function getBaseSkill(actorData, itemData) {
     if (actorData) {
         let baseAbility1 = getAbility(actorData, itemData.data.base1.toLowerCase());
         let baseAbility2 = getAbility(actorData, itemData.data.base2.toLowerCase());
-        return baseAbility1.value + baseAbility2.value;
+        if(baseAbility1 && baseAbility2){
+            return baseAbility1.value + baseAbility2.value;
+        }
     }
 }
 
